5856fed53fc58b3529d956b31f6d76da7c9b2049
Julien Mineraud <julien.mineraud@gmail.com>
2015-12-14 17:20:39 +0200

    Working on authentication for the IoT Hub API

--- test/test-iothub-api.js
+++ test/test-iothub-api.js
@@ -8,19 +8,67 @@ describe("IoT Hub API", function() {
 
 	describe("Authentication", function() {
 
-		var server, port;
+		var app, server, port;
+		var username = 'admin';
+		var password = 'password';
 
-		before(function() {
+		before(function(done) {
 			//The config should set the port to a random number
 			//Create a Role for administrator where only one user belong to
 			//All the db should be set to memory to 
 			var config = {
-				name: randomstring.generate()
+				name: randomstring.generate(),
+				port: 0, // Random port number
+				datasources: {
+					//For the testing we only use the memory
+					"memory": {connector: "memory"},
+					//Set the memory db as default
+					"default": "memory"
+				}
 			};
 
-			var app = iothub(config);
-			server = app.listen();
-			port = server.address().port;
+			app = iothub(config);
+
+			//Need to create one User who is an admin
+			var User = app.models.User;
+			var Role = app.models.Role;
+			var RoleMapping = app.models.RoleMapping;
+
+			var startServer = function() {
+				server = app.listen();
+				port = server.address().port;
+				done();
+			}
+
+			User.create(
+				{username: username, email: username + '@hub.fi', password: password }, 
+				function(err, user) {
+					expect(err).to.not.exist;
+					expect(user).to.exist;
+					
+					//I need to get the admin role
+					Role.findOne(
+						{ where: {name: 'admin'} },
+						function(err, role) {
+							expect(err).to.not.exist;
+							expect(role).to.exist;
+
+							//Assign our user to the role
+							role.principals.create(
+								{
+        							principalType: RoleMapping.USER,
+        							principalId: user.id
+      							}, 
+      							function(err, principal) {
+        							expect(err).to.not.exist;
+									expect(role).to.exist;
+									startServer();
+      							}
+      						);
+						}
+					);			
+				}
+			);
 		});
 
 		after(function() {
@@ -30,47 +78,46 @@ describe("IoT Hub API", function() {
 		it("Non autenticated request should be rejected by default", function(done) {
 			var options = {
                 method: 'GET',
-                uri: 'http://localhost:' + port
+                uri: 'http://localhost:' + port + "/api/feeds/count"
             };
             request.get(options, function(error, response, body) {
-                expect(error).to.exist;
+            	expect(response.statusCode).to.not.equal(200);
                 done();
             });
 		});
 
-		it("Autenticated request by admin", function(done) {
-			var options = {
-                method: 'GET',
-                uri: 'http://localhost:' + port
-            };
-            request.get(options, function(error, response, body) {
-                expect(error).to.exist;
-                done();
-            });
-		})
+		it("Authenticated request by admin", function(done) {
+			
+			var User = app.models.User;
+			var makeRequest = function(token, callback) {
+				var options = {
+                	method: 'GET',
+                	uri: 'http://localhost:' + port + "/api/feeds/count",
+                	headers: {
+        				'Authorization': token
+    				}
+            	};
+            	request.get(options, function(error, response, body) {
+            		expect(response.statusCode).to.equal(200);
+                	callback(token);
+            	});
+			}
 
+			var logout = function(token) {
+				User.logout(token, function (err) {
+     				expect(err).to.not.exist;
+     				done();
+   				});
+			}
 
-	});
-
-	describe("Plugin", function() {
-
-		var server, port;
-
-		before(function() {
-			// runs before all tests in this block
-			var app = iothub();
-			server = app.listen();
-			port = server.address().port;
+			User.login({username: username, password: password},
+				function(err, token) {
+					expect(err).to.not.exist;
+					expect(token).to.exist;
+					makeRequest(token.id, logout);
+				}
+			);   
 		});
-
-		after(function() {
-			server.close();
-		});
-
-		it("", function() {
-
-		});
-
 	});
 
 });
