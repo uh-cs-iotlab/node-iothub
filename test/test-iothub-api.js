bae01a5379d2ac0af737858f57af55c3d11f21f1
Julien Mineraud <julien.mineraud@gmail.com>
2015-12-18 14:37:22 +0200

    Working on executable feed testing

--- test/test-iothub-api.js
+++ test/test-iothub-api.js
@@ -3,72 +3,73 @@ var expect = require('chai').expect,
 	randomstring = require('randomstring'),
 	iothub = require('../lib/iothub');
 
+var app, server, port, username = 'username', password = 'password';
+
+function startServer(done) {
+	//The config should set the port to a random number
+	//Create a Role for administrator where only one user belong to
+	//All the db should be set to memory to 
+	var config = {
+		name: randomstring.generate(),
+		port: 0, // Random port number
+		datasources: {
+			//For the testing we only use the memory
+			"memory": {connector: "memory"},
+			//Set the memory db as default
+			"default": "memory"
+		}
+	};
+
+	app = iothub(config);
+
+	//Need to create one User who is an admin
+	var User = app.models.User;
+	var Role = app.models.Role;
+	var RoleMapping = app.models.RoleMapping;
+
+	var startTheServer = function() {
+		server = app.listen();
+		port = server.address().port;
+		done();
+	}
+
+	User.create(
+		{username: username, email: username + '@hub.fi', password: password }, 
+		function(err, user) {
+			expect(err).to.not.exist;
+			expect(user).to.exist;
+					
+			//I need to get the admin role
+			Role.findOne(
+				{ where: {name: 'admin'} },
+				function(err, role) {
+					expect(err).to.not.exist;
+					expect(role).to.exist;
+
+					//Assign our user to the role
+					role.principals.create(
+						{
+        					principalType: RoleMapping.USER,
+        					principalId: user.id
+      					}, 
+      					function(err, principal) {
+        					expect(err).to.not.exist;
+							expect(role).to.exist;
+							startTheServer();
+      					}
+      				);
+				}
+			);			
+		}
+	);
+}
 
 describe("IoT Hub API", function() {
 
 	describe("Authentication", function() {
 
-		var app, server, port;
-		var username = 'admin';
-		var password = 'password';
-
 		before(function(done) {
-			//The config should set the port to a random number
-			//Create a Role for administrator where only one user belong to
-			//All the db should be set to memory to 
-			var config = {
-				name: randomstring.generate(),
-				port: 0, // Random port number
-				datasources: {
-					//For the testing we only use the memory
-					"memory": {connector: "memory"},
-					//Set the memory db as default
-					"default": "memory"
-				}
-			};
-
-			app = iothub(config);
-
-			//Need to create one User who is an admin
-			var User = app.models.User;
-			var Role = app.models.Role;
-			var RoleMapping = app.models.RoleMapping;
-
-			var startServer = function() {
-				server = app.listen();
-				port = server.address().port;
-				done();
-			}
-
-			User.create(
-				{username: username, email: username + '@hub.fi', password: password }, 
-				function(err, user) {
-					expect(err).to.not.exist;
-					expect(user).to.exist;
-					
-					//I need to get the admin role
-					Role.findOne(
-						{ where: {name: 'admin'} },
-						function(err, role) {
-							expect(err).to.not.exist;
-							expect(role).to.exist;
-
-							//Assign our user to the role
-							role.principals.create(
-								{
-        							principalType: RoleMapping.USER,
-        							principalId: user.id
-      							}, 
-      							function(err, principal) {
-        							expect(err).to.not.exist;
-									expect(role).to.exist;
-									startServer();
-      							}
-      						);
-						}
-					);			
-				}
-			);
+			startServer(done);
 		});
 
 		after(function() {
@@ -76,6 +77,7 @@ describe("IoT Hub API", function() {
 		});
 
 		it("Non autenticated request should be rejected by default", function(done) {
+
 			var options = {
                 method: 'GET',
                 uri: 'http://localhost:' + port + "/api/feeds/count"
@@ -117,6 +119,52 @@ describe("IoT Hub API", function() {
 					makeRequest(token.id, logout);
 				}
 			);   
+		});
+	});
+
+	describe("Executable feeds", function() {
+
+		var token;
+
+		before(function(done) {
+			startServer(function() {
+				var User = app.models.User;
+				User.login({username: username, password: password},
+				function(err, res) {
+					expect(err).to.not.exist;
+					expect(res).to.exist;
+					token = res.id;
+					done();
+				}
+			);   
+			});
+		});
+
+		after(function(done) {
+			var User = app.models.User;
+			User.logout(token, function (err) {
+     			expect(err).to.not.exist;
+     			server.close();
+     			done();
+   			});
+		});
+
+		it("Create a new feed", function(done) {
+			var options = {
+            	method: 'POST',
+                uri: 'http://localhost:' + port + "/api/feeds/",
+                headers: {
+        			'Authorization': token
+    			},
+    			json: {
+    				'named': 'test'
+    			}
+            };
+            request.post(options, function(error, response, body) {
+            	console.log(body);
+            	expect(response.statusCode).to.equal(200);
+                done();
+            });
 		});
 	});
 
