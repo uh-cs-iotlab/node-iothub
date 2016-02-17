39ce0e5ae8a9c0fc43432f6e473c87aa8e4e5526
Aurélien Bertron <aurelien.bertron@insa-lyon.fr>
2016-02-17 18:21:21 +0200

    Add logging middleware

--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var FileStreamRotator = require('file-stream-rotator');
+var morgan = require('morgan');
+var fs = require('fs');
 var app = module.exports = loopback();
+
+var logDirectory = __dirname + '/../logs';
+// ensure log directory exists
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+  filename: logDirectory + '/access-%DATE%.log',
+  frequency: 'daily',
+  verbose: false
+});
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.start = function() {
     // start the web server
