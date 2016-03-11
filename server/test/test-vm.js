a9e1f37e640651596b75174c336585f7ae38cc21
jphire <janne.laukkanen83@gmail.com>
2016-03-11 13:00:13 +0200

    add tests for calling functions inside script in vm

--- server/test/test-vm.js
+++ server/test/test-vm.js
@@ -85,6 +85,156 @@ describe("IoT Hub javascript VM", function () {
         });
     });
 
+    describe("Execute code with function calls in script", function () {
+        it("Do function call inside script", function (done) {
+            var script = [
+                "var r = function(){print('Hello from function')};",
+                "r();"
+            ].join('');
+            var output;
+            var unhook = hook_consolelog(function (msg) {
+                output = msg;
+            });
+            var vm = iothubvm();
+            vm.runScript(script, function (err, res) {
+                unhook();
+                expect(output).to.equal('Hello from function');
+                done();
+            });
+        });
+
+        it("Do function call with arguments inside script", function (done) {
+            var script = [
+                "var message = 'Hello from function';",
+                "var r = function(message){print(message); return 5;};",
+                "r(message);"
+            ].join('');
+            var output;
+            var unhook = hook_consolelog(function (msg) {
+                output = msg;
+            });
+            var vm = iothubvm();
+            vm.runScript(script, function (err, res) {
+                unhook();
+                expect(output).to.equal('Hello from function');
+                expect(res).to.equal(5);
+                done();
+            });
+        });
+
+        it("Run fibonacci algorithm inside script", function (done) {
+            var script = [
+                "function wrapper(){",
+                    "var recursive = function(n) {",
+                        "if(n <= 2) {",
+                            "return 1;",
+                        "} else {",
+                            "return recursive(n - 1) + recursive(n - 2);",
+                        "}",
+                    "};",
+                    "var iterations = [28, 30, 32];",
+                    "var start = null,",
+                        "end = null,",
+                        "time = null,",
+                        "times = [],",
+                        "count = 0;",
+                    "for (var i = 0; i < iterations.length; i++) {",
+                        "count++;",
+                        "start = new Date().getTime();",
+                        "recursive(iterations[i]);",
+                        "end = new Date().getTime();",
+                        "time = end - start;",
+                        "times.push(time);",
+                    "}",
+                    "return count;",
+                "}",
+                "wrapper();"
+            ].join('');
+            var vm = iothubvm();
+            vm.runScript(script, function (err, res) {
+                expect(res).to.equal(3);
+                done();
+            });
+        });
+
+
+        it("Run quicksort algorithm inside script", function (done) {
+            var script = [
+                "function wrapper(){",
+                    "function quickSort(limit) {",
+                        "var nums = [];",
+                        "for (var i = 0; i<limit; i++) {",
+                          "nums[i] = Math.random();",
+                        "}",
+                        "var start = new Date().getTime();",
+                        "nums.sort();",
+                        "var end = new Date().getTime();",
+                        "var time = end - start;",
+                        "return time;",
+                      "};",
+                      "var times = [",
+                        "quickSort(15000),",
+                        "quickSort(20000),",
+                        "quickSort(25000),",
+                      "];",
+                    "return 'Done';",
+                "}",
+                "wrapper();"
+            ].join('');
+            var vm = iothubvm();
+            vm.runScript(script, function (err, res) {
+                expect(res).to.equal('Done');
+                done();
+            });
+        });
+
+        it("Run newton algorithm inside script", function (done) {
+            var script = [
+                "function wrapper(){",
+                    "function sqrt(x) {",
+                        "var count = 8;",
+                        "function iterate(estimate) {",
+                            "count = count - 1;",
+                            "if (count < 1) {",
+                                "return estimate;",
+                            "} else {",
+                                "return iterate((estimate + x/estimate) / 2);",
+                            "}",
+                        "}",
+                        "return iterate(x);",
+                    "}",
+                    "function newton(limit) {",
+                        "var start = null,",
+                            "end = null,",
+                            "time = null,",
+                            "rand = Math.random();",
+                        "start = new Date().getTime();",
+                        "for (var i = 0; i < limit; i++) {",
+                            "sqrt(rand);",
+                        "}",
+                        "end = new Date().getTime();",
+                        "time = end - start;",
+                        "return time;",
+                    "}",
+                    "var iterations = [60000, 80000, 100000],",
+                        "times = [];",
+                    "var len = iterations.length;",
+                    "for (var i = 0; i < len; i++) {",
+                        "time = newton(iterations[i]);",
+                        "times.push(time);",
+                    "}",
+                    "return 'Done';",
+                "}",
+                "wrapper();"
+            ].join('');
+            var vm = iothubvm();
+            vm.runScript(script, function (err, res) {
+                expect(res).to.equal('Done');
+                done();
+            });
+        });
+    });
+
     describe("Execute code with access to XMLHttpRequest", function () {
 
         var server;
