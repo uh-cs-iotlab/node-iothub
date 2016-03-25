94956f7c4dcd545e92166de6c68f0990e40a82a3
jphire <janne.laukkanen83@gmail.com>
2016-03-25 14:21:20 +0200

    remove returnType argument from executable feed
    use Accept header instead of returnType to specify response content type

--- server/test/test-helper.js
+++ server/test/test-helper.js
@@ -206,7 +206,6 @@ module.exports = (app) => {
                 params: [],
                 data: [],
                 description: '',
-                returnType: 'json',
                 readable: false,
                 writeable: false
             }, options);
